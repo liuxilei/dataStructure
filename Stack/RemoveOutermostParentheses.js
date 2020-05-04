d75e4225bebaa34f358a4ee80d600eb1ed3e3d9a
liuxilei <liuxileioo@qq.com>
2020-05-04 17:13:13 +0800

    style: 删除最外层的括号，添加了解题思路

--- Stack/RemoveOutermostParentheses.js
+++ Stack/RemoveOutermostParentheses.js
@@ -1,29 +1,30 @@
 //删除最外层的括号
 
 //我的实现
-
+//解题思路
+//定义一个栈，依次向栈内塞元素，每当（）成对的时候取出，判定栈是否为空，如果为空，即收集到一个原语，然后继续下去。
 /**
  * @param {string} S
  * @return {string}
  */
 var removeOuterParentheses = function (S) {
-    let result = [];
-    let tempList = [];
+    let stack = [];
+    let targetList = [];
+    //记录上次截取的位置
     let primitivePosition = 0;
     for (let i = 0; i < S.length; i++) {
         if (S[i] === "(") {
-            result.push(S[i]);
+            stack.push(S[i]);
         } else {
-            result.pop();
-            if (result.length === 0) {
+            stack.pop();
+            if (stack.length === 0) {
                 let currentPrimitive = S.substring(primitivePosition, i + 1);
-                console.log(currentPrimitive)
                 primitivePosition = i + 1;
-                tempList.push(currentPrimitive.substring(1, currentPrimitive.length - 1));
+                targetList.push(currentPrimitive.substring(1, currentPrimitive.length - 1));
             }
         }
     }
-    return tempList.join("");
+    return targetList.join("");
 };
 
 //others
