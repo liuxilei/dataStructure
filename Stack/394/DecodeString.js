795155f03128a632c1b468be89a06fd62af4bd50
liuxilei <liuxileioo@qq.com>
2020-05-18 06:12:47 +0800

    feat: 栈:字符串解码
    解题思路:顺序迭代字符串,先匹配到数字,进行字符串的累加,当第一次匹配到[字符串时候,把整合好的数字转型并塞入栈内,然后继续匹配如果没遇到[这种符号,就一直向栈中塞入迭代的字符串,当遇到]该字符串时候,通过出栈计算出重复次数,再重新塞入栈,再继续重复以上步骤即可.

--- Stack/394/DecodeString.js
+++ Stack/394/DecodeString.js
@@ -2,10 +2,39 @@
  * @param {string} s
  * @return {string}
  */
-var decodeString = function convertString (str) {
-    const reg = /(\d+)\[([^\[\]]+)\]/g;
-    const res = str.replace(reg, (match, p1, p2) => p2.repeat(p1));
-    return reg.test(res) ? convertString(res) : res;
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var decodeString = function(s) {
+    const isNumber = val => /[0-9]/.test(val);
+    const stack = [];
+    let times = "";
+    for (let i = 0,len = s.length;i < len;i++) {
+        let item = s[i];
+        if (isNumber(item)) {
+            if (i === 0 || isNumber(s[i - 1])) {
+                times += item;
+            } else {
+                times = item;
+            }
+        } else if (item === "[") {
+            stack.push(Number(times));
+            times = "";
+        } else if (item === "]") {
+            let cur = stack.pop();
+            let temp = "";
+            while(typeof cur !== "number") {
+                temp = cur + temp;
+                cur = stack.pop();
+            }
+            temp = temp.repeat(cur);
+            stack.push(temp);
+        } else {
+            stack.push(item);
+        }
+    }
+    return stack.join("");
 };
 
 module.exports = decodeString;
