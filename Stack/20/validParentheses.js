18dad9ec87226068d64716a2cf60067229fe6185
liuxilei <liuxileioo@qq.com>
2020-08-13 06:59:04 +0800

    feat: 栈：有效的括号

--- Stack/20/validParentheses.js
+++ Stack/20/validParentheses.js
@@ -2,18 +2,18 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function (s) {
-    let stack = [];
-    const map = {
-        '[': ']',
-        '{': '}',
-        '(': ')',
+var isValid = function(s) {
+    const stringObj = {
+        "(": ")",
+        "{": "}",
+        "[": "]"
     };
-    for (let i = 0; i < s.length; i++) {
-        if (map[s[i]]) {
-            stack.push(s[i]);
-        } else if (s[i] !== map[stack.pop()]) {
-            return false;
+    const stack = [];
+    for (let item of s) {
+        if (stringObj[stack[stack.length - 1]] === item) {
+            stack.pop();
+        } else {
+            stack.push(item);
         }
     }
     return stack.length === 0;
